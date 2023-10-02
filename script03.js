8674d0646ed2284151fd57f0bcc04bcb49012a7d
Johannes Zelenak <jf.zelenak@gmail.com>
2023-10-02 13:46:47 +0200

    add fizzbuzz exercise

--- script03.js
+++ script03.js
@@ -1,11 +1,30 @@
-for (let i = 1; i < 101; i++) {
-    if (i % 15 === 0) {
-        console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-        console.log("Fizz");
-    } else if (i % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
+// for (let i = 1; i < 101; i++) {
+//     if (i % 15 === 0) {
+//         console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//         console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+function fizzBuzz(start, end) {
+    let answer = "";
+    for (let i = start; i < (end+1); i++) {
+        if (i % 15 === 0) {
+            answer += " FizzBuzz";
+        } else if (i % 3 === 0) {
+            answer += " Fizz";
+        } else if (i % 5 === 0) {
+            answer += " Buzz";
+        } else {
+            answer += " " + i;
+        }
     }
+    return answer;
 }
+
+let message = fizzBuzz(1, 100);
+document.getElementById("solution").innerHTML = message;
