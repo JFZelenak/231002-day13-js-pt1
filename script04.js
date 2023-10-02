5aa9a8ecf4c15515dd9b069bedde4c2cd0a51dbf
Johannes Zelenak <jf.zelenak@gmail.com>
2023-10-02 14:12:03 +0200

    add changes

---
+++ script04.js
@@ -0,0 +1,12 @@
+let message = "";
+
+for (let i = 0; i < 6; i++) {
+    message += "*";
+    // console.log(message);
+    // document.write(message);
+    // document.write("<br>");
+    document.getElementById("solution"+i).innerHTML = message;
+}
+
+
+
